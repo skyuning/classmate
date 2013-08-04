7d003f7ac8e0a65454213ca86c93e834b426d64c
skyun <skyunlin@gmail.com>
2013-08-04 16:50:21 -0700

    Add WelcomeActivity.

--- src/com/example/classmate/Conf.java
+++ src/com/example/classmate/Conf.java
@@ -2,6 +2,6 @@ package com.example.classmate;
 
 public class Conf {
 
-    public static final String HOST = "http://192.168.11.47:8080";
+    public static final String HOST = "http://10.0.2.236:8080";
     public static final String PATH = "/Classmate/app/";
 }
---
+++ src/com/example/classmate/WelcomeActivity.java
@@ -0,0 +1,45 @@
+package com.example.classmate;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        AsyncTask<Void, Void, Void> asyncTask = new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                return null;
+            }
+            
+            @Override
+            protected void onPostExecute(Void result) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        asyncTask.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.welcome, menu);
+        return true;
+    }
+
+}
